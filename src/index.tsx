a0fa50fb34fcc479e53190931d97688d15dc31cf
Emil Holm Gjørup <eg@concordium.com>
2021-01-15 13:18:21 +0100

    Setup react with HMR

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import * as ReactDom from "react-dom";
+import { App } from "./app";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+ReactDom.render(<App />, document.getElementById("app-mount"));
